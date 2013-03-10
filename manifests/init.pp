c515143f052f881d9be4d9aa21fbbfd03c3d43a0
Adam Ochonicki <adam@fromonesrc.com>
2013-03-10 14:23:54 -0400

    latest firefox

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,6 @@
 class firefox {
   package { 'Firefox':
     provider   => 'appdmg',
-    source => 'http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/19.0/mac/en-US/Firefox%2019.0.dmg'
+    source => 'http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/19.0/mac/en-US/Firefox%2019.0.2.dmg'
   }
 }
--- spec/classes/firefox_spec.rb
+++ spec/classes/firefox_spec.rb
@@ -4,6 +4,6 @@ describe 'firefox' do
 
   it { should contain_class('firefox') }
   it { should contain_package('Firefox').with_provider('appdmg') }
-  it { should contain_package('Firefox').with_source('http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/19.0/mac/en-US/Firefox%2019.0.dmg') }
+  it { should contain_package('Firefox').with_source('http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/19.0/mac/en-US/Firefox%2019.0.2.dmg') }
 
 end
