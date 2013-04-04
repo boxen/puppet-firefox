4805602745821fed30d92d996383f939a7227210
Ley Missailidis <moisis.langley@missailidis.ca>
2013-04-04 12:30:11 -0300

    bumps version to 20 and 23 for nightly

--- spec/classes/firefox_spec.rb
+++ spec/classes/firefox_spec.rb
@@ -4,6 +4,6 @@ describe 'firefox' do
 
   it { should contain_class('firefox') }
   it { should contain_package('Firefox').with_provider('appdmg') }
-  it { should contain_package('Firefox').with_source('http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/19.0.2/mac/en-US/Firefox%2019.0.2.dmg') }
+  it { should contain_package('Firefox').with_source('http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/20.0/mac/en-US/Firefox%2020.0.dmg') }
 
 end
