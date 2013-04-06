0b836e039c7702184a1f3c96a7108534fecaf369
Ley Missailidis <moisis.langley@missailidis.ca>
2013-04-06 08:20:21 -0300

    cardboard's up the tests

--- spec/classes/firefox_spec.rb
+++ spec/classes/firefox_spec.rb
@@ -1,9 +1,11 @@
 require 'spec_helper'
 
 describe 'firefox' do
-
-  it { should contain_class('firefox') }
-  it { should contain_package('Firefox').with_provider('appdmg') }
-  it { should contain_package('Firefox').with_source('http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/20.0/mac/en-US/Firefox%2020.0.dmg') }
-
+	it do
+		should contain_class('firefox')
+		should contain_package('Firefox').with({
+			:source   => 'http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/20.0/mac/en-US/Firefox%2020.0.dmg',
+			:provider => 'appdmg'
+		})
+	end
 end
