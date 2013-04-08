de10b43337e0949bc5268dac1903d21aeaa60e2f
Adam Ochonicki <adam@fromonesrc.com>
2013-04-08 19:58:38 -0400

    Clean up whitespace

--- spec/classes/firefox-nightly_spec.rb
+++ spec/classes/firefox-nightly_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'firefox::nightly' do
   it do
-  	should contain_class('firefox::nightly')
+    should contain_class('firefox::nightly')
     should contain_package('Firefox-Nightly').with({
       :source	=>	'http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-trunk/firefox-23.0a1.en-US.mac.dmg',
       :provider	=>	'appdmg'
--- spec/classes/firefox_spec.rb
+++ spec/classes/firefox_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
 describe 'firefox' do
-	it do
-		should contain_class('firefox')
-		should contain_package('Firefox').with({
-			:source	=> 'http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/20.0/mac/en-US/Firefox%2020.0.dmg',
-			:provider	=> 'appdmg'
-		})
-	end
+  it do
+    should contain_class('firefox')
+    should contain_package('Firefox').with({
+      :source	=> 'http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/20.0/mac/en-US/Firefox%2020.0.dmg',
+      :provider	=> 'appdmg'
+    })
+  end
 end
