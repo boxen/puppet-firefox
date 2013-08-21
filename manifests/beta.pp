c390c7dd0f5f5999b1df96acbb8e44487d40cce3
brianvans <brianvans@gmail.com>
2013-08-21 09:40:10 -0600

    updating firefox versions for all classes

--- manifests/beta.pp
+++ manifests/beta.pp
@@ -5,7 +5,7 @@
 #   include firefox::beta
 class firefox::beta ($locale = 'en-US'){
   package { 'Firefox-Beta':
-    source   => "http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/22.0b2/mac/${locale}/Firefox%2022.0b2.dmg",
+    source   => "http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/24.0b4/mac/${locale}/Firefox%2024.0b4.dmg",
     provider => 'appdmg'
   }
 }
--- spec/classes/firefox-ux.rb
+++ spec/classes/firefox-ux.rb
@@ -4,7 +4,7 @@ describe 'firefox::ux' do
   it do
     should contain_class('firefox::ux')
     should contain_package('Firefox-UX').with({
-      :source	=>	'http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-ux/firefox-24.0a1.en-US.mac.dmg',
+      :source	=>	'http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-ux/firefox-26.0a1.en-US.mac.dmg',
       :provider	=>	'appdmg'
     })
   end
