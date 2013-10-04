86a5a65ef5c86a6eca1cb47c0d25e6a1ec3b9683
Maxime Thirouin <m@moox.io>
2013-10-04 18:31:49 +0200

    Update to latests version (2013-10), fix spec

--- spec/classes/firefox-beta.rb
+++ spec/classes/firefox-beta.rb
@@ -4,7 +4,7 @@ describe 'firefox::beta' do
   it do
     should contain_class('firefox::beta')
     should contain_package('Firefox-Beta').with({
-      :source	=> 'http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/24.0b4/mac/en-US/Firefox%2024.0b4.dmg',
+      :source	=> 'http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/25.0b3/mac/en-US/Firefox%2025.0b3.dmg',
       :provider	=> 'appdmg'
     })
   end
