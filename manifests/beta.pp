d21fefaea6f49852a5d04fc15f67e1b1a75fdabf
lonnen <chris.lonnen@gmail.com>
2013-12-16 22:46:40 -0500

    another set of trains have left the station

--- manifests/beta.pp
+++ manifests/beta.pp
@@ -5,7 +5,7 @@
 #   include firefox::beta
 class firefox::beta ($locale = 'en-US'){
   package { 'Firefox-Beta':
-    source   => "http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/26.0b8/mac/${locale}/Firefox%2026.0b8.dmg",
+    source   => "http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/latest-beta/mac/${locale}/Firefox%2027.0b1.dmg",
     provider => 'appdmg'
   }
 }
