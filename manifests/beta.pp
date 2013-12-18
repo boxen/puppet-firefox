26f4f7b8a5cbdbea18b5a440e0ff08a56ec904e3
lonnen <chris.lonnen@gmail.com>
2013-12-18 17:10:13 -0500

    revert to using hardcoded beta path
    ftp.mozilla uses inconsistent naming in its file paths. The alias
    to latest-beta will result in errors when a new beta is released
    (potentially daily) until puppet-firefox has been updated. This
    resolves the issue by reverting the beta path so that it is pinned
    to a specific version. We can rely on Firefox's internal updater
    to keep things fresh in between train switch releases.

--- manifests/beta.pp
+++ manifests/beta.pp
@@ -5,7 +5,7 @@
 #   include firefox::beta
 class firefox::beta ($locale = 'en-US'){
   package { 'Firefox-Beta':
-    source   => "http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/latest-beta/mac/${locale}/Firefox%2027.0b1.dmg",
+    source   => "http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/27.0b2/mac/${locale}/Firefox%2027.0b2.dmg",
     provider => 'appdmg'
   }
 }
