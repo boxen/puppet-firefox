59ed865c8607e8daa3a0a919b81a120a96f779d2
lonnen <chris.lonnen@gmail.com>
2014-05-23 14:43:05 -0700

    update all release channels

--- manifests/beta.pp
+++ manifests/beta.pp
@@ -3,7 +3,7 @@
 # Examples
 #
 #   include firefox::beta
-class firefox::beta ($locale = 'en-US', $version = '29.0b9'){
+class firefox::beta ($locale = 'en-US', $version = '30.0b7'){
   package { 'Firefox-Beta':
     source   => "http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/${version}/mac/${locale}/Firefox%20${version}.dmg",
     provider => 'appdmg'
