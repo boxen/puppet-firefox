279d1b782daa753475ed4261e751f0d1a8ecc5bb
lonnen <chris.lonnen@gmail.com>
2014-05-23 15:04:15 -0700

    fix that thing where we specify the beta version twice in the url for some historic reason

--- spec/classes/firefox-beta_spec.rb
+++ spec/classes/firefox-beta_spec.rb
@@ -4,7 +4,7 @@ describe 'firefox::beta' do
   it do
     should contain_class('firefox::beta')
     should contain_package('Firefox-Beta').with({
-      :source   => 'http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/29.0b9/mac/en-US/Firefox%2030.0b7.dmg',
+      :source   => 'http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/30.0b7/mac/en-US/Firefox%2030.0b7.dmg',
       :provider => 'appdmg'
     })
   end
